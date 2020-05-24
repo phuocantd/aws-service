c28b314d9ab7b063564e600e427058044d8b6fde
phuocantd <phuocantd@gmail.com>
2020-05-24 21:02:32 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import * as AWS from "aws-sdk";
 
 function App() {
+  AWS.config.getCredentials(function (err) {
+    if (err) console.log(err.stack);
+    // credentials not loaded
+    else {
+      console.log("Access key:", AWS.config.credentials.accessKeyId);
+      console.log("Secret access key:", AWS.config.credentials.secretAccessKey);
+    }
+  });
   return (
     <div className="App">
       <header className="App-header">
