7ca64421eeeb73551f915e9de683a4c71e8efd75
phuocantd <phuocantd@gmail.com>
2020-05-25 00:59:43 +0700

    access

--- src/aws/translate.js
+++ src/aws/translate.js
@@ -2,6 +2,8 @@ import * as AWS from "aws-sdk";
 
 export const translate = new AWS.Translate({
   region: process.env.REACT_APP_REGION,
+  secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_KEY,
+  accessKeyId: process.env.REACT_APP_ACCESS_KEY_ID,
 });
 
 // var polly = new AWS.Polly();
